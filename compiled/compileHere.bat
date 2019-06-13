b9eeca167a5d8b86f073aea8a465dc7fc19825c2
schmivin <puterich@gmail.com>
2019-06-13 17:36:36 +0200

    Created a first Compiled Release aswell as . bat files to auto compile new releases

---
+++ compiled/compileHere.bat
@@ -0,0 +1,4 @@
+call C:\Users\puter\OneDrive\Dokumente\Projects\dayZ\loot\Loot\src\compiled\installer.bat
+rmdir /Q/S C:\Users\puter\OneDrive\Dokumente\Projects\dayZ\loot\Loot\src\compiled\build
+DEL /Q  C:\Users\puter\OneDrive\Dokumente\Projects\dayZ\loot\Loot\src\compiled\*.spec
+rmdir /Q/S C:\Users\puter\OneDrive\Dokumente\Projects\dayZ\loot\Loot\src\application\__pycache__
---
+++ compiled/installer.bat
@@ -0,0 +1,4 @@
+cd C:\Users\puter\OneDrive\Dokumente\Projects\dayZ\loot\Loot\src\compiled
+
+pyinstaller --clean --noupx --hidden-import decimal --name DayZ_LootEditor --distpath C:\Users\puter\OneDrive\Dokumente\Projects\dayZ\loot\Loot\src\compiled --path "C:\Program Files (x86)\Windows Kits\10\Redist\ucrt\DLLs\x64" --onefile C:\Users\puter\OneDrive\Dokumente\Projects\dayZ\loot\Loot\src\application\frontend.py
+
