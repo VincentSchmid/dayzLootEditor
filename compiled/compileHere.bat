f88809a9e6eb12b0927021dda7783cd94f44f133
schmivin <puterich@gmail.com>
2019-06-13 20:38:18 +0200

    added Line to compiler batfile

--- compiled/compileHere.bat
+++ compiled/compileHere.bat
@@ -1,4 +1,5 @@
 call C:\Users\puter\OneDrive\Dokumente\Projects\dayZ\loot\Loot\src\compiled\installer.bat
 rmdir /Q/S C:\Users\puter\OneDrive\Dokumente\Projects\dayZ\loot\Loot\src\compiled\build
 DEL /Q  C:\Users\puter\OneDrive\Dokumente\Projects\dayZ\loot\Loot\src\compiled\*.spec
+DEL /Q  C:\Users\puter\OneDrive\Dokumente\Projects\dayZ\loot\Loot\src\compiled\*.7z
 rmdir /Q/S C:\Users\puter\OneDrive\Dokumente\Projects\dayZ\loot\Loot\src\application\__pycache__
