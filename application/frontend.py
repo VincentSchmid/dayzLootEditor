cbad78973a58173acee390a54482c070a2cf07ea
schmivin <puterich@gmail.com>
2019-07-05 00:11:06 +0200

    updated version number

--- application/frontend.py
+++ application/frontend.py
@@ -33,7 +33,7 @@ class Window(object):
     def __init__(self, window):
         self.window = window
         self.checkForDatabase()
-        self.window.wm_title("Loot Editor v0.7")
+        self.window.wm_title("Loot Editor v0.75")
         self.window.protocol("WM_DELETE_WINDOW", self.on_close)
 
         self.changed = False
